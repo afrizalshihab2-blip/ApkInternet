bc5acc3efc6ec42b5ace56faccca01a25a048245
zananneuphoric <zananneuphoric@DESKTOP-0E11R62>
2026-05-02 10:54:18 +0700

    update Teknisi1

--- src/formform/Teknisi1.java
+++ src/formform/Teknisi1.java
@@ -1,22 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package formform;
+import java.sql.*;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.KeyEvent;
+import koneksi.koneksi;
 
-/**
- *
- * @author User
- */
 public class Teknisi1 extends javax.swing.JPanel {
-
-    /**
-     * Creates new form Teknisi1
-     */
+ private Connection conn = new koneksi().connect();
+ private DefaultTableModel tabmode;
+   
     public Teknisi1() {
         initComponents();
+        kosong();
+        aktif();
+        datatable();
     }
+       
+       protected void kosong(){
+        txtid.setText("");
+        txtnm.setText("");
+        txttelp.setText("");
+       
+    }
+    
+    protected void aktif(){
+        txtid.requestFocus();
+    }
+    
+    protected void datatable(){
+        Object[] Baris ={"ID Teknisi","Nama Teknisi","No.Telepon"};
+          tabmode = new DefaultTableModel(null, Baris);
+         
+        try{
+            String sql = "SELECT * FROM teknisi where id_teknisi order by id_teknisi asc";
+            Statement stat = conn.createStatement();
+            ResultSet hasil = stat.executeQuery(sql);
+            while (hasil.next()){
+                tabmode.addRow(new Object[]{
+                    hasil.getString(1),
+                    hasil.getString(2),
+                    hasil.getString(3),
+                  
+                });
+               }
+               tblteknisi.setModel(tabmode);
+        }catch (Exception e) {
+                JOptionPane.showMessageDialog(null,"data gagal dianggil"+e);
+        }
+    }
+    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -28,40 +60,48 @@ public class Teknisi1 extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel10 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        jButtonHapus = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField1 = new javax.swing.JTextField();
-        jButton2 = new javax.swing.JButton();
+        txtnm = new javax.swing.JTextField();
+        txtid = new javax.swing.JTextField();
+        jButtonBatal = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jTextField3 = new javax.swing.JTextField();
+        tblteknisi = new javax.swing.JTable();
+        txttelp = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
-        jButton4 = new javax.swing.JButton();
+        jButtonTambah = new javax.swing.JButton();
         jLabel9 = new javax.swing.JLabel();
 
-        setBackground(new java.awt.Color(204, 204, 204));
+        setBackground(new java.awt.Color(204, 204, 255));
 
         jLabel10.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
-        jLabel10.setText("No Telp");
+        jLabel10.setText("No. Telp :");
 
-        jButton1.setBackground(new java.awt.Color(153, 0, 153));
-        jButton1.setText("Hapus");
+        jButtonHapus.setBackground(new java.awt.Color(255, 255, 153));
+        jButtonHapus.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButtonHapus.setText("Hapus");
+        jButtonHapus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonHapusActionPerformed(evt);
+            }
+        });
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 28)); // NOI18N
         jLabel1.setText("Data Teknisi");
 
-        jButton2.setBackground(new java.awt.Color(255, 51, 51));
-        jButton2.setText("Batal");
-        jButton2.setSelected(true);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        jButtonBatal.setBackground(new java.awt.Color(255, 204, 204));
+        jButtonBatal.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButtonBatal.setText("Batal");
+        jButtonBatal.setSelected(true);
+        jButtonBatal.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                jButtonBatalActionPerformed(evt);
             }
         });
 
-        jTable1.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        tblteknisi.setBackground(new java.awt.Color(204, 204, 255));
+        tblteknisi.setFont(new java.awt.Font("Times New Roman", 0, 18)); // NOI18N
+        tblteknisi.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null},
                 {null, null, null},
@@ -75,16 +115,27 @@ public class Teknisi1 extends javax.swing.JPanel {
                 "ID Teknisi", "Nama", "No Hp"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        tblteknisi.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblteknisiMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tblteknisi);
 
         jLabel8.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
-        jLabel8.setText("ID Teknisi");
+        jLabel8.setText("ID Teknisi :");
 
-        jButton4.setBackground(new java.awt.Color(0, 255, 0));
-        jButton4.setText("Tambah");
+        jButtonTambah.setBackground(new java.awt.Color(153, 255, 153));
+        jButtonTambah.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButtonTambah.setText("Tambah");
+        jButtonTambah.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonTambahActionPerformed(evt);
+            }
+        });
 
         jLabel9.setFont(new java.awt.Font("Segoe UI", 1, 16)); // NOI18N
-        jLabel9.setText("Nama");
+        jLabel9.setText("Nama :");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -94,25 +145,23 @@ public class Teknisi1 extends javax.swing.JPanel {
                 .addGap(253, 253, 253)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(4, 4, 4)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel10)
-                                .addGap(33, 33, 33))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel9)
-                                    .addComponent(jLabel8))
-                                .addGap(18, 18, 18)))
+                            .addComponent(jLabel9)
+                            .addComponent(jLabel8)
+                            .addComponent(jLabel10))
+                        .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtnm, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txtid, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txttelp, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton4)
-                                .addGap(18, 18, 18)
-                                .addComponent(jButton1)
-                                .addGap(18, 18, 18)
-                                .addComponent(jButton2))))
+                                .addGap(28, 28, 28)
+                                .addComponent(jButtonTambah)
+                                .addGap(81, 81, 81)
+                                .addComponent(jButtonHapus)
+                                .addGap(77, 77, 77)
+                                .addComponent(jButtonBatal))))
                     .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 244, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 730, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(377, Short.MAX_VALUE))
@@ -127,41 +176,126 @@ public class Teknisi1 extends javax.swing.JPanel {
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel8)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtid, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtnm, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel9))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txttelp, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel10))
                 .addGap(26, 26, 26)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton4)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(jButtonTambah)
+                    .addComponent(jButtonHapus)
+                    .addComponent(jButtonBatal))
                 .addContainerGap(252, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    private void jButtonBatalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonBatalActionPerformed
+       kosong();
+        datatable();
+    }//GEN-LAST:event_jButtonBatalActionPerformed
 
+    private void jButtonTambahActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonTambahActionPerformed
+     String sql = "insert into teknisi values (?,?,?)";
+        try{
+            PreparedStatement stat = conn.prepareStatement(sql);
+            stat.setString(1, txtid.getText());
+            stat.setString(2, txtnm.getText());
+            stat.setString(3, txttelp.getText());
+            
+            
+            stat.executeUpdate();
+            JOptionPane.showMessageDialog(null, "data berhasil disimpan");
+            kosong();
+            txtid.requestFocus();
+        }
+        catch (SQLException e){
+            JOptionPane.showMessageDialog(null,"data gagal disimpan"+e);
+        }
+        datatable();
+    
+    }//GEN-LAST:event_jButtonTambahActionPerformed
+
+    private void jButtonHapusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonHapusActionPerformed
+       int ok = JOptionPane.showConfirmDialog(null,"hapus","konfirmasi dialog", JOptionPane.YES_NO_OPTION);
+           if (ok==0){
+               String sql = "delete from teknisi where id_teknisi ='"+txtid.getText()+"'";
+               try{
+                   PreparedStatement stat = conn.prepareStatement(sql);
+                   stat.executeUpdate();
+                   JOptionPane.showMessageDialog(null, "data berhasil di hapus");
+                   kosong();
+                   txtid.requestFocus();
+               }
+               catch (SQLException e){
+                   JOptionPane.showMessageDialog(null,"data gagal dihapus"+e);
+               }
+               datatable();
+           }
+    }//GEN-LAST:event_jButtonHapusActionPerformed
+
+    private void tblteknisiMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblteknisiMouseClicked
+       int bar = tblteknisi.getSelectedRow();
+        String a = tabmode.getValueAt(bar, 0).toString();
+        String b = tabmode.getValueAt(bar, 1).toString();
+        String c = tabmode.getValueAt(bar, 2).toString();
+        
+        
+        txtid.setText(a);
+        txtnm.setText(b);
+      txttelp.setText(c);
+     
+    }//GEN-LAST:event_tblteknisiMouseClicked
+   public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Teknisi1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Teknisi1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Teknisi1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Teknisi1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Teknisi1().setVisible(true);
+            }
+        });
+    }
+   
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButtonBatal;
+    private javax.swing.JButton jButtonHapus;
+    private javax.swing.JButton jButtonTambah;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTable tblteknisi;
+    private javax.swing.JTextField txtid;
+    private javax.swing.JTextField txtnm;
+    private javax.swing.JTextField txttelp;
     // End of variables declaration//GEN-END:variables
 }
