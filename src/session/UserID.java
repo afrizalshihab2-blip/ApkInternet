b4c1a828dde0aa009871c7459b56ca43ef329fc2
chntyaprr <chintyaaputrii00@gmail.com>
2026-06-25 13:08:25 +0700

    fixx Signed-off-by:chntyaprr <chintyaaputrii00@gmail.com>

---
+++ src/session/UserID.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package session;
+public class UserID {  
+    private static String kd;
+    
+    public static void setUserLogin(String kode){
+        UserID.kd = kode;
+    }
+    
+    public static String getUserLogin(){
+        return kd;
+    }
+}
