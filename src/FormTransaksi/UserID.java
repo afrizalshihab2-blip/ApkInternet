b691e4b1696e2027140c2db1d770e16ffb789039
Lenovo <Lenovo@DESKTOP-DOO5O43>
2026-06-30 23:41:10 +0700

    Transaksi_Fix

---
+++ src/FormTransaksi/UserID.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FormTransaksi;
+
+import session.*;
+
+
+class UserID {
+    private static String kd;
+    
+    public static void setUserLogin(String kode){
+        
+        UserID.kd = kode;
+    }
+    
+    public static String getUserLogin(){
+        return kd;
+    }
+}
